56a61aaff681ded8df64850b6f7d976f2df1ff62
Shubham Singhal <shubham.011190@gmail.com>
2020-07-29 14:39:08 +0530

    Update

--- routes/api/users.js
+++ routes/api/users.js
@@ -34,9 +34,7 @@ router.post('/register', (req, res) => {
 
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if (err) {
-                            console.log(err);;
-                        }
+                        if (err) console.log(err);
                         newUser.password = hash;
                         newUser.save()
                             .then(user => res.json(user))

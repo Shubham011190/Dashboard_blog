350784513ceba54b7d094cb9ba6cc27776681264
Shubham Singhal <shubham.011190@gmail.com>
2020-07-29 15:10:53 +0530

    Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -29,7 +29,7 @@ router.post('/register', (req, res) => {
                     name: req.body.name,
                     email: req.body.email,
                     avatar,
-                    password : req.body.password,
+                    password: req.body.password,
                 });
 
                 bcrypt.genSalt(10, (err, salt) => {
@@ -41,7 +41,36 @@ router.post('/register', (req, res) => {
                             .catch(err => console.log(err))
                     })
                 })
-        }
-    })
-})
+            }
+        })
+});
+
+//@route    GET api/users/login
+//@desc     Login user/ Returning JWT Token
+//@access   Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //Find user by email
+    User.findOne({ email })
+        .then(user => {
+            //Check for user
+            if (!user) {
+                return res.status(404).json({ email: "User not found" });
+            }
+
+            //Check password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({ msg: "Success" })
+                    } else {
+                        return res.status(400).json({ password: "Password incorrect." })
+                    }
+                })
+        })
+});
+
+
 module.exports = router;

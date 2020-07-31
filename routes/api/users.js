b39d1cc826de188907c93d9aa1a3a4e0cb6929c2
Shubham Singhal <shubham.011190@gmail.com>
2020-07-31 14:56:55 +0530

    Login Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 
 //Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 //@route    GET api/users/test
 //@desc     Tests users route
@@ -63,6 +64,12 @@ router.post('/register', (req, res) => {
 //@desc     Login user/ Returning JWT Token
 //@access   Public
 router.post('/login', (req, res) => {
+     const { errors, isValid } = validateLoginInput(req.body);
+    
+    //Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
     const email = req.body.email;
     const password = req.body.password;
 
@@ -71,7 +78,8 @@ router.post('/login', (req, res) => {
         .then(user => {
             //Check for user
             if (!user) {
-                return res.status(404).json({ email: "User not found" });
+                errors.email = 'User not found.'
+                return res.status(404).json(errors);
             }
 
             //Check password
@@ -94,7 +102,8 @@ router.post('/login', (req, res) => {
                                 });
                         });
                     } else {
-                        return res.status(400).json({ password: "Password incorrect." })
+                        errors.password = 'Password Incorrect'
+                        return res.status(400).json(errors);
                     }
                 })
         })
